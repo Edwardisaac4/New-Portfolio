ab79b250e2f1606f1db1ae8178ac0b7c56e386f2
Edwardisaac4 <isaacdrumz4@gmail.com>
2026-04-01 10:58:06 +0100

    feat: implement 3D hero model component with React Three Fiber and add global styles and UI components

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+
+
+// eslint-disable-next-line no-unused-vars
+const Button = ({className, id, text}) => {
+    return (
+        <a href="" className={`${className ?? ''} cta-wrapper`}>
+            <div className="cta-button group">
+                <div className="bg-circle" />
+                <p className="text">{text}</p>
+                <div className="arrow-wrapper">
+                    <img src="/images/arrow-down.svg" alt="arrow" />
+                </div>
+            </div>
+        </a>
+    )
+}
+
+export default Button
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,6 @@
+import Button from "../components/Button.jsx"
 import { words } from "../constants/index.js"
+import HeroExperience from "../components/HeroModels/HeroExperience.jsx"
 const Hero = () => {
     return (
         <section id="hero" className = "relative overflow-hidden">
@@ -8,7 +10,7 @@ const Hero = () => {
 
             <div className="hero-layout">
                 {/*LEFT CONTENT OF THE HERO SECTION*/}
-                <header className = "flex flex-col md:w-full justify-center w-screen md:px-18 px-44">
+                <header className = "flex flex-col md:w-full justify-center w-screen md:px-18 px-4">
                     <div className="flex flex-col gap-5">
                         <div className="hero-text">
                             <h1>Shaping 
@@ -32,9 +34,23 @@ const Hero = () => {
                             <h1>Into Real Projects</h1>
                             <h1>That Deliver Results</h1>
                         </div>
+
+                        <p className="pointer-events-none text-white-50 relative z-9 md:text-xl text-lg">
+                            I'm Isaac, A Front End Developer with a passion for <br />building innovative and user-friendly web applications
+                        </p>
+                        
+                        <Button
+                            className="md:w-78 md:h-14 w-57 h-10"
+                            id="button"
+                            text="See My Work"
+                        />
                     </div>
                 </header>
-
+                
+                {/*RIGHT CONTENT OF THE HERO SECTION*/}
+                <figure className="hero-3d-layout">
+                    <HeroExperience />
+                </figure>
             </div>
 
 
